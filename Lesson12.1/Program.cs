02fccd776c7af91374d2cbb8aef28afdcfb6638e
rakdav <rakdav@inbox.ru>
2024-10-09 21:25:56 +0200

    Lesson12.1

---
+++ Lesson12.1/Program.cs
@@ -0,0 +1,11 @@
+﻿using Lesson12._1;
+
+Student<int>[] mas = new Student<int>[3];
+mas[0]=new Student<int>("Vasia",16);
+mas[1] = new Student<int>("Boris",13 );
+mas[2] = new Student<int>("Eugen", 10);
+Array.Sort(mas);
+for (int i = 0; i < mas.Length; i++)
+{
+    Console.WriteLine(mas[i]);
+}
--- Lesson12/Program.cs
+++ Lesson12/Program.cs
@@ -1,19 +1,37 @@
-﻿Person p1=new Person();
-Person p2 = new Person()
-{
-    Name = "Vasia",
-    Age = 34
-};
-Console.WriteLine(p1.Name);
-Console.WriteLine(p1.Age);
-p1.Move();
-p1.Eat();
-Console.WriteLine(IMovable.minSpeed);
-Console.WriteLine(IMovable.maxSpeed);
+﻿//Person p1=new Person();
+//Person p2 = new Person()
+//{
+//    Name = "Vasia",
+//    Age = 34
+//};
+//Console.WriteLine(p1.Name);
+//Console.WriteLine(p1.Age);
+//p1.Move();
+//p1.Eat();
+//Console.WriteLine(IMovable.minSpeed);
+//Console.WriteLine(IMovable.maxSpeed);
 
-Car car = new Car() { Name = "Mersedes", Age = 5 };
-car.Move();
-car.Eat();
+//Car car = new Car() { Name = "Mersedes", Age = 5 };
+//car.Move();
+//car.Eat();
+
+TaskManager taskManager = new TaskManager();
+taskManager.Add(new Task()
+{
+    DueDate = DateTime.Parse("12.02.2024"),
+    Priority = Priority.Medium,
+    Title = "t1"
+});
+taskManager.Add(new Event()
+{
+    Title = "e1",
+    Priority = Priority.High,
+    DueDate = DateTime.Parse("9.10.2024"),
+    Location="Kenig"
+});
+taskManager.Print();
+taskManager.Remove(taskManager.Tasks[1]);
+taskManager.Print();
 class Person : IMovable, IEating
 {
     private string name; 
@@ -70,6 +88,73 @@ class Car : IMovable,IEating
         Console.WriteLine("Пью все, что горит!!!");
     }
 }
+
+class Task : ITask
+{
+    public string Title { get; set; }
+    public DateTime DueDate { get; set; }
+    public Priority Priority { get; set; }
+    public void Display()
+    {
+        Console.WriteLine(Title+" "+DueDate+" "+Priority);
+    }
+}
+class Event : ITask
+{
+    public string Location { get; set; }
+    public string Title { get; set; }
+    public DateTime DueDate { get; set; }
+    public Priority Priority { get; set; }
+
+    public void Display()
+    {
+        Console.WriteLine( Location+" "+Title+" "+DueDate+" "+Priority);
+    }
+}
+
+class TaskManager
+{
+    public List<ITask> Tasks { get; set; } = new();
+    public void Add(ITask task)
+    {
+        Tasks.Add(task);
+    }
+    public void Remove(ITask task)
+    {
+        Tasks.Remove(task);
+    }
+    public void Print()
+    {
+        foreach (var item in Tasks)
+        {
+            item.Display();
+        }
+    }
+}
+class ImplementationInt : IMyInterface<int>
+{
+    private int a;
+    public int GetValue()
+    {
+        return a;
+    }
+    public void SetValue(int value)
+    {
+        a = value;
+    }
+}
+class ImplementationString : IMyInterface<string>
+{
+    private string str;
+    public string GetValue()
+    {
+        return str;
+    }
+    public void SetValue(string value)
+    {
+        str = value;
+    }
+}
 interface IMovable
 {
     const int minSpeed = 0;
@@ -93,4 +178,10 @@ interface ITask
     DateTime DueDate { get; set; }
     Priority Priority { get; set; }
     void Display();
+}
+
+interface IMyInterface<T>
+{
+    T GetValue();
+    void SetValue(T value);
 }
